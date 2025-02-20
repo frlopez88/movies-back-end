8a8a1bf79dc47d0db9e988f6f5607b1bb354a313
frlopez88 <frlopez88@icloud.com>
2025-02-19 20:35:04 -0600

    Auth Validation Middleware

--- index.js
+++ index.js
@@ -4,23 +4,40 @@ import { movies } from './routes/movieRoute.js';
 import { actor } from './routes/actorRoute.js';
 import { earnings } from './routes/earningsRoute.js';
 import { authUser } from './routes/authRoute.js';
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const myMiddleWare = (req, res, next)=>{
-    console.log("Enter to the middleware")
-    const data =  req.body
-    console.log(req.body)
-    next()
+const tokenValidation =(req, res, next)=>{
+
+    const authorization = req.headers['authorization']
+    if (!authorization){
+        return res.status(400).json({message : "You need to pass a Token"})
+    }
+
+    const token = authorization.replace('Bearer ', '')
+
+    try {
+        const secret = process.env.KEY_SECRET
+        const decodeToken = jwt.verify(token, secret)
+        next()
+    }catch(err){
+        return res.status(400).json({message : "Invalid Token"})
+    }
+    
+
+    
 }
 
 
 // Routes
-app.use('/movies', myMiddleWare, movies);
-app.use('/actors', actor);
-app.use('/earnings', earnings);
-app.use('/auth', authUser)
+app.use('/movies', tokenValidation , movies);
+app.use('/actors', tokenValidation, actor);
+app.use('/earnings', tokenValidation, earnings);
+app.use('/auth', authUser);
 
 app.listen(3001);
